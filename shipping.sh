e422c0a9e6e8e3d4f1297893f5f390380fdeaf2f
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-16 02:19:29 +0530

    shell-roboshop-project updated

--- shipping.sh
+++ shipping.sh
@@ -73,9 +73,9 @@ validate $? "Installing mysql"
 
 mysql -h $mysql_host -u root -p RoboShop@1 -e 'use cities' &>>$log_file
 if [ $? -ne 0 ]; then
-    mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/schema.sql &>>$log_file
-    mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$log_file
-    mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$log_file
+    mysql -h $mysql_host -uroot -pRoboShop@1 < /app/db/schema.sql &>>$log_file
+    mysql -h $mysql_host -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$log_file
+    mysql -h $mysql_host -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$log_file
     validate $? "Loaded data to mysql"
 else
     echo -e "Data already loaded to mysql ...$Y SKIPPING$N" | tee -a $log_file
