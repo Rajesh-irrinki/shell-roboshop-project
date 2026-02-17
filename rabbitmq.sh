d2a77a6d2d1b35969be4bdf536fbfe03541ebdf8
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-17 17:13:30 +0530

    shell-roboshop-project updated

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -9,7 +9,7 @@ Y="\e[33m"
 N="\e[0m"
 
 if [ $user_id -ne 0 ]; then
-    echo "$R Please run the script with root access $N"
+    echo -e "$R Please run the script with root access $N"
     exit 1
 fi
 
@@ -17,10 +17,10 @@ mkdir -p $log_folder
 
 validate() {
     if [ $1 -ne 0 ]; then
-        echo "$2 ... $R FAILED $N" | tee -a $log_file
+        echo -e "$2 ... $R FAILED $N" | tee -a $log_file
         exit 1
     else
-        echo "$2 ... $G SUCCESS $N" | tee -a $log_file  
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $log_file  
     fi 
 }
 
@@ -30,13 +30,13 @@ validate $? "Created Systemctl service"
 dnf install rabbitmq-server -y &>>$log_file
 validate $? "Installing rabbitmq-server"
 
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl enable rabbitmq-server &>>$log_file
+systemctl start rabbitmq-server &>>$log_file
 validate $? "Enabling and Starting rabittmq-server"
 
-rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl add_user roboshop roboshop123 &>>$log_file
 validate $? "Add user"
 
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$log_file
 validate $? "set permissions for user"
 
