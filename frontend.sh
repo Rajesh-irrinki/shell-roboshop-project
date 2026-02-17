21e7721dc64cd74458b8654019ca27b84f4d5fd0
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-18 03:40:16 +0530

    shell-roboshop-project updated

--- frontend.sh
+++ frontend.sh
@@ -6,6 +6,8 @@ log_file=$log_folder/$0.sh
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
+script_dir=$PWD
 
 if [ $user_id -ne 0 ]; then
     echo -e "$R Please run the script with root access $N" | tee -a $log_file
@@ -16,10 +18,10 @@ mkdir -p $log_folder
 
 validate () {
     if [ $1 -ne 0 ] ; then
-        echo "$2 ...$R FAILED $N" | tee -a $log_file
+        echo -e "$2 ...$R FAILED $N" | tee -a $log_file
         exit 1
     else
-        echo "$2 ...$G SUCCESS $N" | tee -a $log_file
+        echo -e "$2 ...$G SUCCESS $N" | tee -a $log_file
 }
 
 dnf module disable nginx -y &>>$log_file
@@ -38,11 +40,17 @@ validate $? "Enable and start Nginx service"
 rm -rf /usr/share/nginx/html/* &>>$log_file
 validate $? "Removing existing Nginx configuration"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$log_file
 validate $? "Downloading the Frontend code"
 
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html &>>$log_file
+validate $? "Moving to nginx configuration directory"
+
+unzip /tmp/frontend.zip &>>$log_file
 validate $? "Unzipping the Frontend code"
 
-sed -i ""
+cp $script_dir /etc/nginx/nginx.conf &>>$log_file
+validate $? "Reverse Proxy config creation"
+
+systemctl restart nginx &>>$log_file
+validate $? "Nginx service restarted"
