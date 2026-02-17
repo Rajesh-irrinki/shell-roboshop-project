14a48d95b2f8618d0f04556e78b54cf627bf8044
rajeshirrinki <rajeshirrinki321@gmail.com>
2026-02-17 17:08:51 +0530

    shell-roboshop-project updated

--- shipping.sh
+++ shipping.sh
@@ -19,10 +19,10 @@ mkdir -p $log_folder
 
 validate() {
     if [ $1 -ne 0 ]; then
-        echo "$2 ... $R FAILED $N"
+        echo -e "$2 ... $R FAILED $N"
         exit 1
     else
-        echo "$2 ... $G SUCCESS $N"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
@@ -78,7 +78,7 @@ if [ $? -ne 0 ]; then
     mysql -h $mysql_host -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$log_file
     validate $? "Loaded data to mysql"
 else
-    echo -e "Data already loaded to mysql ...$Y SKIPPING $N" | tee -a $log_file
+    echo -e "Data already loaded to mysql ... $Y SKIPPING $N" | tee -a $log_file
 fi
 
 systemctl restart shipping &>>$log_file
